838e179f98b2f80ee6dbb0c76b3ef10b14365565
softone-as <72479238+softone-as@users.noreply.github.com>
2020-10-07 15:46:11 +0700

    Add files via upload

---
+++ bab3/src/bab3/ch1/Listing.java
@@ -0,0 +1,16 @@
+/*
+ * Shofwan AS
+ * 191511031
+ * D3-2A
+ */
+
+package bab3.ch1;
+
+//write code for task
+public class Listing {
+    
+    public static void main(String[] args){
+        System.out.println("We will not use 'Hello, World!'");
+    }
+
+}
