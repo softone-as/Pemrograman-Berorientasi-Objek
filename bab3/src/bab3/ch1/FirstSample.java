bdf66aca3f03092caf68db414b971e194391586e
softone-as <72479238+softone-as@users.noreply.github.com>
2020-10-07 14:34:55 +0700

    Add files via upload

---
+++ bab3/src/bab3/ch1/FirstSample.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bab3.ch1;
+
+/**
+ *
+ * @author USER
+ */
+public class FirstSample {
+    
+    public static void main(String[] args){
+        System.out.println("We will not use 'Hello, World!'");
+    }
+}
