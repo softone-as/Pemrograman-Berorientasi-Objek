143952855d1f1b93b66fe2d374d0e76f47403d1e
softone-as <72479238+softone-as@users.noreply.github.com>
2020-10-08 08:43:05 +0700

    Add files via upload

---
+++ bab3/src/bab3/ch1/Boolean.java
@@ -0,0 +1,19 @@
+
+package bab3.ch1;
+
+
+public class Boolean {
+    
+    public static void main(String[] args){
+        
+        boolean myBoolean = false;
+           
+        if(myBoolean){
+            System.out.println("the value is false");    
+        }else{
+            System.out.println("that's true");
+        }
+        
+    }
+    
+}
