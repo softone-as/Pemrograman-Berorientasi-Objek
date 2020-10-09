4785d17737edee89c37d0ffac0678b769edf1eee
softone-as <72479238+softone-as@users.noreply.github.com>
2020-10-09 14:28:40 +0700

    Add files via upload

---
+++ bab3/src/bab3/ch5/MathematicalFunctionConstant.java
@@ -0,0 +1,15 @@
+
+package bab3.ch5;
+
+
+public class MathematicalFunctionConstant {
+    
+    public static void main(String[] args){
+        double x = 4;
+        double y = Math.sqrt(x);
+        double c = Math.PI;
+        
+        System.out.println(y); // prints 2.0
+        System.out.println(c); // prints 3.14
+    }
+}
