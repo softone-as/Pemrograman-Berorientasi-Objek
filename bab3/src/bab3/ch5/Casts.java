836fd1d0c2def61368b66e85f7e2bf3f36c31d14
softone-as <72479238+softone-as@users.noreply.github.com>
2020-10-10 22:25:44 +0700

    Add files via upload

---
+++ bab3/src/bab3/ch5/Casts.java
@@ -0,0 +1,16 @@
+
+package bab3.ch5;
+
+
+public class Casts {
+    
+    public static void main(String[] args){
+        double x = 9.997;
+        int nx = (int) x;
+        int nz = (int)Math.round(x);
+        
+        System.out.println(nx);
+        System.out.println(nz);
+    }
+    
+}
