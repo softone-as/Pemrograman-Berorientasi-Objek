1e486adf1e91824f9831b8a37bd87480c3144f35
softone-as <72479238+softone-as@users.noreply.github.com>
2020-10-11 07:46:13 +0700

    Add files via upload

---
+++ bab3/src/bab3/ch6/Substring.java
@@ -0,0 +1,15 @@
+
+package bab3.ch6;
+
+
+public class Substring {
+    
+    public static void main(String[] args){
+    String greeting = "Hello";
+    String s = greeting.substring(0, 3);
+
+    System.out.println(s);
+    }
+    
+    
+}
