dd2386103e2825a62e50be1ef4d9f1688cd676f6
softone-as <72479238+softone-as@users.noreply.github.com>
2020-10-13 15:21:42 +0700

    Add files via upload

---
+++ bab3/src/bab3/ch5/BitwiseOperator.java
@@ -0,0 +1,40 @@
+
+package bab3.ch5;
+
+
+public class BitwiseOperator {
+    
+    public static void main(String[] args){
+        
+        int a = 3; //00000011
+        int b = 24; //00011000
+        int c = 12; //00001100
+        int result1, result2, result3, result4, result5, result6;
+        
+        //Shift Left
+        result1 = (a << 3); //00011000 = 24
+        
+        //Shift Right
+        result2 = (b >> 2);//00000110 = 6
+        
+        //Or
+        result3 = (b | c); //00011100 = 20 
+        
+        //And
+        result4 = (b & c); //00001000 = 8
+        
+        //XOR
+        result5 = (b ^ c); //00010100 = 20
+        
+        //NOT
+        result6 = (~b); // 00011000
+        
+        System.out.println("a << 3 = " + result1);
+        System.out.println("b >> 2 = " + result2);
+        System.out.println("b | c = " + result3);
+        System.out.println("b & c = " + result4);
+        System.out.println("b ^ c = " + result5);
+        System.out.println("~b = " + result6);
+    }
+    
+}
